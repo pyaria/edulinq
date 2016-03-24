7d8f4153a20716d4a162f70fc214eb1a93adc553
JadeTong <jade@hellosmartcompany.com>
2016-03-24 11:40:09 -0700

    Range implemented!

--- src/Edulinq/Range.cs
+++ src/Edulinq/Range.cs
@@ -23,24 +23,18 @@ namespace Edulinq
     {
         public static IEnumerable<int> Range(int start, int count)
         {
-            if (count < 0)
-            {
-                throw new ArgumentOutOfRangeException("count");
-            }
-            // Convert everything to long to avoid overflows. There are other ways of checking
-            // for overflow, but this way make the code correct in the most obvious way.
-            if ((long)start + (long)count - 1L > int.MaxValue)
-            {
-                throw new ArgumentOutOfRangeException("count");
-            }
+            if (count < 0) throw new ArgumentOutOfRangeException("count");
+            if ((long)count + (long)start -1L > int.MaxValue) throw new ArgumentOutOfRangeException("count");
+   
             return RangeImpl(start, count);
         }
-
         private static IEnumerable<int> RangeImpl(int start, int count)
         {
-            for (int i = 0; i < count; i++)
+            var index = 0;
+            while (index < count)
             {
-                yield return start + i;
+                yield return start;
+                index++; start++;
             }
         }
     }

e2452a65b2873de3958de3552207cffe53a17c84
JadeTong <jade@hellosmartcompany.com>
2016-03-31 10:46:38 -0700

    repeat implemented!

--- src/Edulinq/Empty.cs
+++ src/Edulinq/Empty.cs
@@ -24,60 +24,16 @@ namespace Edulinq
     {
         public static IEnumerable<TResult> Empty<TResult>()
         {
-            return EmptyEnumerable<TResult>.Instance;
+            var dictionary = new Dictionary<Type, Type[]>();
+            
+            foreach (Type type in dictionary.Keys)
+            {
+                //if (type == TResult) return dictionary[type];
+                var a = type.Name;
+            }
+            return new TResult[] { };
         }
 
-#if AVOID_RETURNING_ARRAYS
-        private class EmptyEnumerable<T> : IEnumerable<T>, IEnumerator<T>
-        {
-            internal static IEnumerable<T> Instance = new EmptyEnumerable<T>();
 
-            // Prevent construction elsewhere
-            private EmptyEnumerable()
-            {
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return this;
-            }
-
-            IEnumerator IEnumerable.GetEnumerator()
-            {
-                return this;
-            }
-
-            public T Current
-            {
-                get { throw new InvalidOperationException(); }
-            }
-
-            object IEnumerator.Current
-            {
-                get { throw new InvalidOperationException(); }
-            }
-
-            public void Dispose()
-            {
-                // No-op
-            }
-
-            public bool MoveNext()
-            {
-                return false; // There's never a next entry
-            }
-
-            public void Reset()
-            {
-                // No-op
-            }
-        }
-
-#else
-        private static class EmptyEnumerable<T>
-        {
-            internal static readonly T[] Instance = new T[0];       
-        }
-#endif
     }
 }
--- src/Edulinq/Repeat.cs
+++ src/Edulinq/Repeat.cs
@@ -20,18 +20,18 @@ namespace Edulinq
 {
     public static partial class Enumerable
     {
-        public static IEnumerable<TResult> Repeat<TResult>(TResult element, int count)
+        public static IEnumerable<TResult> Repeat<TResult>(
+            TResult element, 
+            int count)
         {
-            if (count < 0)
-            {
-                throw new ArgumentOutOfRangeException("count");
-            }
+            if (count < 0) throw new ArgumentOutOfRangeException("count");
             return RepeatImpl(element, count);
         }
-
-        private static IEnumerable<TResult> RepeatImpl<TResult>(TResult element, int count)
+        private static IEnumerable<TResult> RepeatImpl<TResult>(
+            TResult element,
+            int count)
         {
-            for (int i = 0; i < count; i++)
+            for (var i = 0; i < count; i++)
             {
                 yield return element;
             }
